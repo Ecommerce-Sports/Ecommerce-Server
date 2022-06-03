894ca39623501fba37e701a6a4114ea015b010ca
cahyaseptia11 <cahyaseptia011@gmail.com>
2022-06-03 07:47:32 +0700

    add index routes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const routes = express.Router();
 const kategori_routes = require("./kategori_routes")
+const customer_routes = require("./customer_routes")
+const pembayaran_routes = require("./pembayaran_routes")
+const pesanan_routes = require("./pesanan_routes")
+
 
 routes.use("/kategori", kategori_routes)
+routes.use("/customer", customer_routes)
+routes.use("/pembayaran", pembayaran_routes)
+routes.use("/pesanan", pesanan_routes)
 
 module.exports = routes
