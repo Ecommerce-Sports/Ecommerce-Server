37c32824a7c78f5b4aa7228605138fa7a630a84a
budibrass <immortalbudi@yahoo.com>
2022-07-05 20:01:25 +0700

    revisi code for deploy heroku

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ if(process.env.NODE_ENV == 'development') {
 
 const express = require("express");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const route = require("./routes");
 const bodyParser = require("body-parser");
 const Cors = require("cors");
