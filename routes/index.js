0ae66ae6931f2d241575417a69b67f797239c050
budibrass <immortalbudi@yahoo.com>
2022-07-05 21:22:11 +0700

    add message in route /

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,9 @@ const csats_routes = require("./csats_routes");
 const upload_routes = require("./upload_routes");
 const banner_routes = require("./banner_routes");
 const rajaongkir_routes = require("./rajaongkir_routes");
+const { route } = require("./categories_routes");
 
+route.get("/", (req, res) => res.status(200).json({ message: "welcome to ecommerce sports admin server" }))
 routes.use("/user", user_routes);
 routes.use("/cart", carts_routes);
 routes.use("/csat", csats_routes);
